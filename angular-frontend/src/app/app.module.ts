2d1f74c9416f03edefc539e968a3fd961fe67981
MKA1500 <maciejkabsch@gmail.com>
2019-03-26 21:34:35 +0100

    replacing services parts with reducer

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { CoreModule } from './core/core.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducers';
 
 
 @NgModule({
@@ -18,7 +21,10 @@ import { CoreModule } from './core/core.module';
     ShoppingListModule,
     SharedModule,
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      shoppingList: shoppingListReducer
+    })
   ],
   bootstrap: [AppComponent]
 })
